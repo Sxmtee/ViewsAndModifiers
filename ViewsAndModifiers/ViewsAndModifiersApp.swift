41bfc8b3e4fe2a6e9318a4f9c42e6e93db4cea5c
sxmtee <somtoo76@gmail.com>
2024-04-16 22:53:04 +0100

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by mac on 16/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
