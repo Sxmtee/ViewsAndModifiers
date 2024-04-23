36945ea703eb755a9ae04e46b19dab167ad30f5d
sxmtee <somtoo76@gmail.com>
2024-04-23 10:53:33 +0100

    Update

---
+++ ViewsAndModifiers/rockPaperScissors.swift
@@ -0,0 +1,18 @@
+//
+//  rockPaperScissors.swift
+//  ViewsAndModifiers
+//
+//  Created by mac on 19/04/2024.
+//
+
+import SwiftUI
+
+struct RockPaperScissors: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#Preview {
+    RockPaperScissors()
+}
